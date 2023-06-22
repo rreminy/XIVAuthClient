2f52936661febad0e4e87d20993559efc7981dec
Reminy Light <rreminy@gmail.com>
2023-06-22 01:22:52 -0400

    Fix CharacterUpdateModel - Ignore content_id property when writing default

--- XIVAuthClient/Models/CharacterUpdateModel.cs
+++ XIVAuthClient/Models/CharacterUpdateModel.cs
@@ -5,6 +5,7 @@ namespace XIVAuth.Models
     public sealed class CharacterUpdateModel
     {
         [JsonPropertyName("content_id")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public ulong? ContentId { get; init; }
     }
 }
